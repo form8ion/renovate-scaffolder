9e6886bcff732c00097ffc949f50b43300cfd235
Matt Travi <programmer@travi.org>
2023-08-27 23:41:02 -0500

    wip(badge): extracted the badge definition to a separate scaffolder

---
+++ src/badges/scaffolder.js
@@ -0,0 +1,13 @@
+export default function () {
+  return {
+    badges: {
+      contribution: {
+        renovate: {
+          text: 'Renovate',
+          link: 'https://renovatebot.com',
+          img: 'https://img.shields.io/badge/renovate-enabled-brightgreen.svg?logo=renovatebot'
+        }
+      }
+    }
+  };
+}
---
+++ src/badges/scaffolder.test.js
@@ -0,0 +1,15 @@
+import {describe, expect, it} from 'vitest';
+
+import scaffoldBadges from './scaffolder';
+
+describe('badges scaffolder', () => {
+  it('should define the renovate badge', () => {
+    const {badges} = scaffoldBadges();
+
+    expect(badges.contribution.renovate).toEqual({
+      text: 'Renovate',
+      link: 'https://renovatebot.com',
+      img: 'https://img.shields.io/badge/renovate-enabled-brightgreen.svg?logo=renovatebot'
+    });
+  });
+});
