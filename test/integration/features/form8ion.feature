a229085d204fc12815b7254a3cf74076cab0d6d7
Matt Travi <programmer@travi.org>
2024-07-26 14:24:59 -0400

    test(plugin): pin behavior of scaffolder producing content that is detected by predicate

--- test/integration/features/form8ion.feature
+++ test/integration/features/form8ion.feature
@@ -3,3 +3,4 @@ Feature: renovate-scaffolder as form8ion plugin
   Scenario: plugin conventions
     When the renovate plugin is compared to form8ion plugin conventions
     Then the public interface is compatible with the plugin schema
+    And the output produced by the scaffolder is detectable by the predicate
--- test/integration/features/step_definitions/form8ion-steps.js
+++ test/integration/features/step_definitions/form8ion-steps.js
@@ -1,14 +1,19 @@
 import {validateOptions, optionsSchemas} from '@form8ion/core';
 
 import {Then, When} from '@cucumber/cucumber';
+import {assert} from 'chai';
 
 // eslint-disable-next-line import/no-extraneous-dependencies,import/no-unresolved
 import * as plugin from '@form8ion/renovate-scaffolder';
 
 When('the renovate plugin is compared to form8ion plugin conventions', async function () {
-  return undefined;
+  await plugin.scaffold({projectRoot: this.projectRoot});
 });
 
 Then('the public interface is compatible with the plugin schema', async function () {
   validateOptions(optionsSchemas.form8ionPlugin, plugin);
+});
+
+Then('the output produced by the scaffolder is detectable by the predicate', async function () {
+  assert.isTrue(await plugin.test({projectRoot: this.projectRoot}));
 });
